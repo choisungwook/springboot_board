71e13034c4c4e2a82012043ff5c5bccef8122ad2
choisungwook <kgg1959@naver.com>
2020-10-31 15:01:52 +0900

    Add 게시글 Entity

---
+++ src/main/java/study/board/domain/Post.java
@@ -0,0 +1,47 @@
+package study.board.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "POSTS")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends TimeBaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "post_id")
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private Long hit;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bbs_id")
+    private BBS bbs;
+
+    @Builder
+    public Post(String title, String content, BBS bbs) {
+        this.title = title;
+        this.content = content;
+        this.hit = 0L;
+
+        // 연관관계 설정
+        if(bbs != null){
+            this.bbs = bbs;
+            bbs.add_post(this);
+        }
+    }
+
+    public void increase_hit(){
+        hit += 1;
+    }
+}
